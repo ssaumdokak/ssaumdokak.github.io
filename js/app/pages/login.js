3e80a07c8c9879f567b291de3cbc28495ee649be
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-01-28 14:02:53 +0100

    first

---
+++ js/app/pages/login.js
@@ -0,0 +1,20 @@
+export const login = {
+    data() {
+        return {
+            img: 1,
+            hs: 0,
+            parent: "",
+        };
+    },
+    mounted: function () {
+       
+    },
+    methods: {
+       
+    },
+    template: `
+    <div class="flex">
+        fhcntfalcnuirs
+    </div>
+`,
+};
