a5d85a53767c0aca65aa5f2f1bed28f0dc57bd9f
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:12:07 +0100

    5

--- js/app/app.js
+++ js/app/app.js
@@ -1,154 +1,102 @@
-import { router } from "./router.js";
-import { msg } from "./widgets/msg.js";
-import { popup } from "./widgets/popup.js";
-import { header } from "./widgets/header.js";
-import { toogle } from "./widgets/toogle.js";
-import { img } from "./widgets/img.js";
+import { router } from './router.js';
+import { header } from './widgets/header.js';
+// import { search } from './widgets/search.js';
+import { popup } from './widgets/popup.js';
+import { msg } from './widgets/msg.js';
+import { toogle } from './widgets/toogle.js';
+import { img } from './widgets/img.js';
 
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
     const main = {
         data() {
             return {
                 url: "https://affiliate.yanbasok.com",
-                user: {
-                    name: "",
-                    phone: "",
-                    email: "",
-                    date: "",
-                    auth: "",
-                },
+                user: {name:"", phone:"", email:"", date:"", auth:""},
                 formData: {},
                 title: "",
                 date: "",
                 time: "",
-            };
+            }
         },
+
         watch: {
-            $route() {
+            $route:function() {
                 this.init();
-            },
+            }
         },
-        mounted() {
+
+        mounted:function() {
             this.init();
         },
+
         methods: {
             init() {
-                const self = this;
-
-                if (window.localStorage.getItem("user")) {
-                    self.user = JSON.parse(window.localStorage.getItem("user"));
-                }
+                var self = this;
+                if (window.localStorage.getItem('user')) self.user = JSON.parse(window.localStorage.getItem('user'));
 
                 router.isReady().then(() => {
-                    if (window.localStorage.getItem("user")) {
-                        self.user = JSON.parse(
-                            window.localStorage.getItem("user")
-                        );
-
-                        if (
-                            self.$route.path === "/" &&
-                            self.user.type === "admin"
-                        ) {
-                            self.page("/campaigns");
-                        } else if (
-                            [
-                                "/campaigns",
-                                "/campaign",
-                                "/users",
-                                "/user",
-                            ].includes(self.$route.path) &&
-                            self.user.type !== "admin"
-                        ) {
-                            self.page("/statistics");
-                        } else if (
-                            ["/statistics", "/payments", "/sites"].includes(
-                                self.$route.path
-                            ) &&
-                            self.user.type === "admin"
-                        ) {
-                            self.page("/campaigns");
-                        } else if (
-                            [
-                                "/campaigns",
-                                "/campaign",
-                                "/users",
-                                "/user",
-                                "/statistics",
-                                "/payments",
-                                "/sites",
-                            ].includes(self.$route.path)
-                        ) {
+                    if (window.localStorage.getItem('user')) {
+                        self.user = JSON.parse(window.localStorage.getItem('user'));
+                        if (self.$route['path'] == '/' && self.user.type == 'admin') {
+                            self.page('/campaigns');
+                        } else if (['/campaigns', '/campaign', '/users', '/user'].includes(self.$route['path']) && self.user.type != 'admin') {
+                            self.page('/statistics');
+                        } else if (['/statistics', '/payments', '/sites'].includes(self.$route['path']) && self.user.type == 'admin') {
+                            self.page('/campaigns');
+                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'])) {
                             self.page();
-                        } else if (
-                            ![
-                                "/campaigns",
-                                "/campaign",
-                                "/users",
-                                "/user",
-                                "/statistics",
-                                "/payments",
-                                "/sites",
-                            ].includes(self.$route.path)
-                        ) {
+                        } else if (!['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'])) {
                             self.page();
                         }
                     } else {
-                        self.page("/");
+                        self.page('/');
                     }
                 });
             },
 
             logout() {
-                this.user = {
-                    name: "",
-                    phone: "",
-                    email: "",
-                    date: "",
-                    auth: "",
-                };
-                this.page("/");
-                window.localStorage.setItem("user", "");
+                this.user = {name:"", phone:"", email:"", date:"", auth:""};
+                this.page('/');
+                window.localStorage.setItem('user', '');
             },
 
-            page(path = "") {
+            page:function(path = "") {
                 this.$router.replace(path);
-                this.title = this.$route.name;
-                document.title = this.$route.name;
+                this.title = this.$route['name'];
+                document.title = this.$route['name'];
             },
 
-            toFormData(obj) {
-                const fd = new FormData();
-                for (const x in obj) {
-                    if (
-                        typeof obj[x] === "object" &&
-                        x !== "img" &&
-                        x !== "copy"
-                    ) {
-                        for (const y in obj[x]) {
-                            if (typeof obj[x][y] === "object") {
-                                for (const z in obj[x][y]) {
-                                    fd.append(`${x}[${y}][${z}]`, obj[x][y][z]);
+            toFormData:function(obj) {
+                var fd = new FormData();
+
+                for(var x in obj) {
+                    if (typeof obj[x] === 'object' && x != 'img' && x != 'copy') {
+                        for(var y in obj[x]) {
+                            if (typeof obj[x] === 'object') {
+                                for(var z in obj[x][y]) {
+                                    fd.append(x + '[' + y + '][' + z + ']', obj[x][y][z]);
                                 }
                             } else {
-                                fd.append(`${x}[${y}]`, obj[x][y]);
+                                fd.append(x + '[' + y + ']', obj[x][y]);
                             }
                         }
-                    } else if (x !== "copy") {
+                    } else if (x != 'copy') {
                         fd.append(x, obj[x]);
                     }
                 }
+
                 return fd;
-            },
-        },
+            }
+        }
     };
 
-    Vue.createApp(main)
-        .component("Image", img)
-        .component("Header", header)
-        .component("toogle", toogle)
-        .component("msg", msg)
-        .component("popup", popup)
-        .use(router)
-        .mount("#content");
+    var app = Vue.createApp(main)
+    .component('Header', header)
+    .component('popup', popup)
+    // .component('search', search)
+    .component('msg', msg)
+    .component('toogle', toogle)
+    .component('Image', img)
+    .use(router)
+    .mount('#content')
 });
