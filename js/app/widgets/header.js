f784b55b67bbcfe201f2c24963910c5861f71423
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:12:43 +0100

    5

--- js/app/widgets/header.js
+++ js/app/widgets/header.js
@@ -1,92 +1,62 @@
 export const header = {
-    data: function () {
+    data:function() {
         return {
             user: {},
             parent: "",
             active: 0,
-            menu: 0,
-        };
+            menu: 0
+        }
     },
-    watch: {},
+    watch: {
+
+    },
     mounted() {
         this.parent = this.$parent.$parent.$parent;
     },
     methods: {
         toogleActive() {
-            this.active = this.active === 1 ? 0 : 1;
-        },
+            if (this.active == 1) {
+                this.active = 0;
+            } else {
+                this.active = 1;
+            }
+        }
     },
     template: `
         <header class="header">
             <div class="wrapper">
-                <div class="flex">
-                    <div class="w20 logo">
-                        <img :src="parent.url + '/app/views/images/logo.svg'" />
+                <div class="flex header-content">
+                    <div class="w20 logo ">
+                        <img :src="parent.url+'/app/views/images/logo.svg'" />
                     </div>
-                    <div class="w70">
+                    <div class="w70 h-inh">
                         <div id="menu">
-                            <ul :class="{ active: menu == 1 }" v-if="parent.user && parent.user.type && parent.user.type == 'admin'">
-                                <li v-if="menu == 1" class="a1">
-                                    <i class="fas fa-times" @click="menu = 0"></i>
-                                </li>
-                                <li>
-                                    <router-link
-                                        :class="{ 'router-link-active': $route.path.search('campaign') == 1 }"
-                                        to="/campaigns"
-                                    >
-                                        <i class="fas fa-bullhorn"></i> Campaigns
-                                    </router-link>
-                                </li>
-                                <li>
-                                    <router-link
-                                        :class="{ 'router-link-active': $route.path.search('user') == 1 }"
-                                        to="/users"
-                                    >
-                                        <i class="fas fa-users"></i> Users
-                                    </router-link>
-                                </li>
+                            <ul :class="{active:menu == 1}" v-if="parent.user && parent.user.type && parent.user.type=='admin'">
+                                <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
+                                <li><router-link :class="{'router-link-active':$route.path.search('campaign')==1}" to="/campaigns"><p>Campaigns <i class="fas fa-bullhorn"></i></p></li>
+                                <li><router-link :class="{'router-link-active':$route.path.search('user')==1}" to="/users"><p>Users <i class="fas fa-users"></i></p></li>
                             </ul>
-                            <ul :class="{ active: menu == 1 }" v-if="parent.user && parent.user.type && parent.user.type != 'admin'">
-                                <li v-if="menu == 1" class="a1">
-                                    <i class="fas fa-times" @click="menu = 0"></i>
-                                </li>
-                                <li>
-                                    <router-link to="/statistics">
-                                        <i class="fas fa-chart-area"></i> Statistics
-                                    </router-link>
-                                </li>
-                                <li>
-                                    <router-link to="/ads">
-                                        <i class="fas fa-image"></i> Ads
-                                    </router-link>
-                                </li>
-                                <li>
-                                    <router-link to="/sites">
-                                        <i class="fab fa-chrome"></i> Sites
-                                    </router-link>
-                                </li>
-                                <li>
-                                    <router-link to="/payments">
-                                        <i class="fas fa-credit-card"></i> Payments
-                                    </router-link>
-                                </li>
+
+                            <ul :class="{active:menu == 1}" v-if="parent.user && parent.user.type && parent.user.type!='admin'">
+                                <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
+                                <li><router-link to="/statistics"><i class="fas fa-chart-area"></i> Statistics</router-link></li>
+                                <li><router-link to="/ads"><i class="fas fa-image"></i> Ads</router-link></li>
+                                <li><router-link to="/sites"><i class="fas fa-chrome"></i> Sites</router-link></li>
+                                <li><router-link to="/payments"><i class="fas fa-credit-card"></i> Payments</router-link></li>
                             </ul>
                         </div>
                     </div>
-                    <div class="w10 a1" id="user-top" v-if="parent.user && parent.user.user">
-                        <div id="user-circle" @click="toogleActive()">
-                            {{ parent.user.user[0] }}
-                        </div>
-                        <div id="user-info" :class="{ active: active == 1 }">
-                            <a href="#" @click.prevent="parent.logout()">
-                                <i class="fas fa-sign-out-a1t"></i> {{ parent.user.user }} Log out
-                            </a>
-                        </div>
+                    <div class="w10 al" id="user-top" v-if="parent.user && parent.user.user">
+                        <div id="user-circle" @click="toogleActive()">{{parent.user.user[0]}}</div>
                         <i @click="toogleActive()" class="fas fa-caret-down"></i>
+
+                        <div id="user-info" :class="{active:active == 1}">
+                            <a href="#" @click.prevent="parent.logout();"><i class="fas fa-sign-out-alt"></i> {{parent.user.user}} Log out</a>
+                        </div>
                     </div>
                 </div>
             </div>
             <msg ref="msg" />
         </header>
-    `,
-};
+    `
+}
