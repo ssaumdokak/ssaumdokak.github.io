bd3bfe28f705cd70b9422c4e04454289d912a890
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:13:12 +0100

    4

--- js/app/widgets/msg.js
+++ js/app/widgets/msg.js
@@ -1,24 +1,26 @@
 export const msg = {
-    data() {
+    data:function() {
         return {
             alert: "",
             success: "",
             t1: "",
             t2: "",
+            confirmTitle: "Please confirm next action",
+            confirm: "",
             code: 0,
             interval: "",
-            confirmTitle: "", 
-            confirm: "",
-        };
+        }
     },
-    watch: {},
+    watch: {
+
+    },
     mounted() {
         this.parent = this.$parent.$parent.$parent;
     },
     methods: {
         fadeIn(el, timeout, display) {
             el.style.opacity = 0;
-            el.style.display = display || "block";
+            el.style.display = display || 'block';
             el.style.transition = `opacity ${timeout}ms`;
             setTimeout(() => {
                 el.style.opacity = 1;
@@ -28,55 +30,58 @@ export const msg = {
             el.style.opacity = 1;
             el.style.transition = `opacity ${timeout}ms`;
             el.style.opacity = 0;
+
             setTimeout(() => {
-                el.style.display = "none";
+                el.style.display = 'none';
             }, timeout);
         },
         successFun(msg) {
             this.success = msg;
+
             var self = this;
-            if (document.querySelector(".successMsg"))
-                document.querySelector(".successMsg").style = "";
+            if (document.querySelector('.successMsg')) document.querySelector('.successMsg').style = "";
             clearTimeout(self.t1);
             clearTimeout(self.t2);
-            self.t1 = setTimeout(function () {
-                const block = document.querySelector(".successMsg");
-                self.fadeIn(block, 1000, "flex");
-                self.t2 = setTimeout(function () {
+            self.t1 = setTimeout(function() {
+                const block = document.querySelector('.successMsg');
+                self.fadeIn(block, 1000, 'flex');
+                self.t2 = setTimeout(function() {
                     self.fadeOut(block, 1000);
                 }, 3000);
             }, 100);
         },
-
         alertFun(msg) {
+            console.log(msg);
             this.alert = msg;
+
             var self = this;
-            if (document.querySelector(".alertMsg"))
-                document.querySelector(".alertMsg").style = "";
+            if (document.querySelector('.alertMsg')) document.querySelector('.alertMsg').style = "";
             clearTimeout(self.t1);
             clearTimeout(self.t2);
-            self.t1 = setTimeout(function () {
-                const block = document.querySelector(".alertMsg");
-                self.fadeIn(block, 1000, "flex");
-                self.t2 = setTimeout(function () {
+            self.t1 = setTimeout(function() {
+                const block = document.querySelector('.alertMsg');
+                console.log(block);
+                self.fadeIn(block, 1000, 'flex');
+                self.t2 = setTimeout(function() {
                     self.fadeOut(block, 1000);
                 }, 3000);
-            }, 100);
+            }, 500);
         },
-
         confirmFun(title, text) {
             this.code = 0;
             var self = this;
-            return new Promise(function (resolve, reject) {
+
+            return new Promise(function(resolve, reject) {
                 self.confirmTitle = title;
                 self.confirm = text;
                 self.$refs.confirm.active = 1;
-                self.interval = setInterval(function () {
+                self.interval = setInterval(function() {
                     if (self.code > 0) resolve();
                 }, 100);
-            }).then(function () {
+            }).then(function() {
                 clearInterval(self.interval);
                 self.$refs.confirm.active = 0;
+
                 if (self.code == 1) {
                     return true;
                 }
@@ -84,33 +89,27 @@ export const msg = {
                     return false;
                 }
             });
-        },
+        }
     },
     template: `
-    <div>
-        <div class="alertMsg" v-if="alert"> 
-            <div class="wrapper al"> 
-                <i class="fas fa-times-circle"></i> {{ alert }} 
-            </div> 
-        </div> 
-        <div class="successMsg" v-if="success"> 
-            <div class="wrapper al"> 
-                <i class="fas fa-check-circle"></i> {{ success }} 
-            </div> 
+        <div class="alertMsg" v-show="alert">
+            <div class="wrapper al">
+                <span><i class="fas fa-times-circle"></i> {{alert}}</span>
+            </div>
         </div>
-    </div>
-
-    <popup ref="confirm" :title="confirmTitle"> 
-      <div class="al">
-        <div class= "ab1">
-          {{confirm}}  <i class="fas fa-info-circle"></i> 
+        <div class="successMsg" v-show="success">
+            <div class="wrapper al">
+                <span><i class="fas fa-check-circle"></i> {{success}}</span>
+            </div>
         </div>
-        <div class="botBtns"> 
-          <a class="btnSS" href="#" @click.prevent="code=1">Yes</a> 
-          <a class="btnSS" href="#" @click.prevent="code=2">No</a> 
-        </div> 
-      </div> 
-    </popup>
-    
-    `,
+        <popup ref="confirm" :title="confirmTitle">
+            <div class="al popup-text prl20 ptb20">
+                <span><i class="fas fa-info-circle"></i> {{confirm}}</span>
+                <div class="botBtns">
+                    <a class="btnS" href="#" @click.prevent="code=1">Yes</a>
+                    <a class="btnS" href="#" @click.prevent="code=2">No</a>
+                </div>
+            </div>
+        </popup>
+    `
 };
