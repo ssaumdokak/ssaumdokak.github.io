98c17bb44778b372a559dcb94b1ccadf95751d00
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:15:23 +0100

    2

--- js/app/pages/login.js
+++ js/app/pages/login.js
@@ -1,81 +1,80 @@
 export const login = {
-    data() {
+    data:function() {
         return {
             img: 1,
-            parent: "",
-        };
+            parent: ''
+        }
     },
-    mounted: function () {
+    mounted:function() {
         this.img = this.randomIntFromInterval(1, 7);
         this.parent = this.$parent.$parent;
     },
     methods: {
-        randomIntFromInterval: function (min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
+        randomIntFromInterval:function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min)
         },
-        login: function () {
+        login:function() {
             var self = this;
             var data = self.parent.toFormData(self.parent.formData);
-            axios
-                .post(this.parent.url + "/site/login", data)
-                .then(function (response) {
-                    if (response.data.error) {
-                        self.$refs.msg.alertFun(response.data.error);
-                    }
-                    if (response.data.user) {
-                        self.parent.user = response.data.user;
-                        self.parent.page("/campaigns");
-                        window.localStorage.setItem(
-                            "user",
-                            JSON.stringify(self.parent.user)
-                        );
-                    }
-                })
-                .catch(function (error) {
-                    console.log("errors: ", error);
-                });
+
+            console.log(self.parent.toFormData(self.parent.formData));
+
+            axios.post(this.parent.url + "/site/login", data).then(function(response) {
+                if (response.data.error) {
+                    self.$refs.msg.alertFun(response.data.error);
+                }
+                if (response.data.user) {
+                    self.parent.user = response.data.user;
+                    self.parent.page('/campaigns');
+                    window.localStorage.setItem('user', JSON.stringify(self.parent.user));
+                }
+            }).catch(function(error) {
+                console.log('errors: ', error);
+            });
         },
+
     },
     template: `
-        <div class="flex"> 
-            <msg ref="msg"/> 
-            <div id="left-area" class="w60"> 
-                <img :src="parent.url + '/app/views/images/Cover_' + img + '.jpg'" /> 
-            </div> 
-            <div id="right-area" class="w40"> 
-                <div class="headers"> 
-                    <div class="wrappers flex"> 
-                    <div class="w60 al1"> 
-                            <h1>Affiliate Sign in</h1> 
-                        </div> 
-                        <div class="w40 logo"> 
-                            <img :src="parent.url + '/app/views/images/logo.svg'" /> 
-                        </div> 
-                        
-                    </div> 
-                </div>
-                <div class="forms inner-form p20"> 
-                    <form @submit.prevent="login" v-if="parent.formData"> 
-                        <div class="row"> 
-                            <label>Email</label> 
-                            <input type="email" v-model="parent.formData.email" required> 
-                        </div> 
-                        <div class="row"> 
-                            <label>Password</label> 
-                            <input 
-                                type="password" 
-                                v-model="parent.formData.password" 
-                                required 
-                                autocomplete="on"
-                            > 
+        <div class="flex page">
+            <msg ref="msg"/>
+            <div id="right-area" class="w40">
+                <div class="header">
+                    <div class="wrapper flex">
+                        <div class="logo">
+                            <img :src="parent.url+'/app/views/images/logo.svg'" />
                         </div>
-                        <div class="row"> 
-                            <button class="btn">Sign in</button> 
-                        </div> 
-                    </form> 
-                </div> 
-            </div> 
+                        <div class="al">
+                            <h1>Affiliate Sign in</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form inner-form flex">
+                    <form @submit.prevent="login()" v-if="parent.formData">
+                        <div class="row">
+                            <label>
+                                Email
+                                <input type="email" name="email" v-model="parent.formData.email" required>
+                            </label>
+                            
+                        </div>
+
+                        <div class="row">
+                            <label>
+                                Password
+                                <input type="password" name="password" v-model="parent.formData.password" required autocomplete="on">
+                            </label>
+                        </div>
+
+                        <div class="row">
+                            <button class="btn">SIGN IN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             
+            <div id="left-area" class="w60">
+                <img :src="parent.url+'/app/views/images/Cover_'+img+'.jpg'" />
+            </div>
         </div>
-    `,
-};
+`};
