094be7c2b68f598fa8e990d45aeffd1c6fbcbc34
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:12:58 +0100

    5

--- js/app/widgets/img.js
+++ js/app/widgets/img.js
@@ -1,60 +1,43 @@
 export var img = {
-    data() {
-      return {
-        value: "",
-      };
+    data:function() {
+        return {
+            value: ""
+        }
     },
     mounted() {
-        if (this.modelValue && typeof this.modelValue === "string") {
-          this.value = this.parentUrl + "/" + this.modelValue;
+        this.parent = this.$parent.$parent.$parent.$parent;
+        if (this.modelValue != undefined) {
+            this.value = this.parent.url + '/' + this.modelValue;
         }
-      },
-    computed: {
-      parentUrl() {
-        const parent = this.$parent.$parent.$parent.$parent;
-        return parent ? parent.url : "";
-      },
     },
     methods: {
         change(event) {
-          const file = event.target.files[0];
-          const reader = new FileReader();
-          reader.onload = () => {
-            this.value = reader.result;
-          };
-          this.$emit("update:modelValue", file);
-        },
-      },
-    watch: {
-      modelValue(newVal) {
-        if (newVal && typeof newVal === "string") {
-          this.value = this.parentUrl + "/" + newVal;
+            var self = this;
+            var file = event.target.files[0];
+            var reader = new FileReader();
+
+            reader.onload = function() {
+                self.value = reader.result;
+            };
+
+            this.$emit('update:modelValue', event.target.files[0]);
         }
-        else if (!newVal) {
-          this.value = "";
-        }
-      },
     },
     props: {
-      modelValue: String,
+        modelValue: String
     },
+
     template: `
-      <div class="image-preview-area">
-        <a href="#" class="select_img" @click.prevent="$refs.input.click()">
-            <span v-if="value">
-            <img :src="value" :class="['im', 'new-image']">
-            </span>
-            <span v-else>
-            <img :src="parentUrl + '/app/views/images/placeholder.png'" class="im">
-            </span>
-        </a>
+        <div class="image-preview-area">
+            <a href="#" class="select_img" @click.prevent="$refs.input.click()">
+                <span v-if="value">
+                    <img :src="value" class="im" />
+                </span>
+                <span v-if="!value">
+                    <img :src="this.parent.url + '/app/views/images/placeholder.png'">
+                </span>
+            </a>
         </div>
-        <input 
-              type="file" 
-              data-name="image" 
-              ref="input" 
-              accept="image/jpeg, image/png, image/gif, image/webp, image/svg+xml" 
-              @change="change($event)" 
-              class="file-input">
-    `,
-  };
+        <input type="file" data-name="image" ref="input" accept="image/jpeg, image/png, image/gif, image/webp, image/svg+xml" @change="change($event)" />
+    `
+};
