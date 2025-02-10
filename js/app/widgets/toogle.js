5ba39991f00c56ad699202d74046c7f961047208
dao <146765250+ssaumdokak@users.noreply.github.com>
2025-02-10 12:13:35 +0100

    4

--- js/app/widgets/toogle.js
+++ js/app/widgets/toogle.js
@@ -1,29 +1,30 @@
 export var toogle = {
-    data: function () {
+    data:function() {
         return {
-            value: "",
-        };
+            value: ""
+        }
     },
     watch: {
-        modelValue: function (o, n) {
+        modelValue:function(o, n) {
             this.value = this.modelValue;
-        },
+        }
     },
     mounted() {
         this.value = this.modelValue;
     },
     methods: {
         change() {
-            this.$emit("update:modelValue", this.value.toString());
-        },
+            this.$emit('update:modelValue', this.value.toString());
+        }
     },
     props: {
-        modelValue: String,
+        modelValue: String
     },
+
     template: `
-        <label class="switch"> 
-            <input type="checkbox" v-model="value" @change="change()"> 
-            <span class="slider round"></span> 
-        </label> 
-    `,
+        <label class="switch">
+            <input type="checkbox" v-model="value" @change="change()">
+            <span class="slider round"></span>
+        </label>
+    `
 };
